0aab391ff4c67d3bb4c581f5fd546feec5b83be5
Whitney <169212050+whitneybarr@users.noreply.github.com>
2024-07-31 17:15:48 -0500

    Add functionality and basic styling

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # phrasal-template-game
+
+A phrasal template game in the style of "MadLibs." 
+
+I am a beginner to JavaScript, currently working through free resources such as The Odin Project, JavaScript.info and MDN Docs, and I wanted a somewhat simple project to learn from, that I could share with family and friends when I finished. JavaScript Libs occured to me as a project that I could implement with my limited knowledge, while still having to research new methods as I went along. The project also allowed me to get some more practice in HTML and CSS. 
---
+++ script.js
@@ -0,0 +1,15 @@
+
+
+function writeStory() {
+    let adjective1 = document.getElementById("adjective1").value;
+    let profession = document.getElementById("profession").value;
+    let verbing = document.getElementById("verbing").value;
+    let container = document.getElementById("container").value;
+    let adjective2 = document.getElementById("adjective2").value;
+    let shape = document.getElementById("shape").value;
+    let adjective3 = document.getElementById("adjective3").value;
+    let smallAnimal = document.getElementById("smallAnimal").value;
+    let adjective4 = document.getElementById("adjective4").value;
+
+    document.getElementById("filledTemplate").innerHTML = 'Once upon a time, a ' + adjective1 +  " " + profession + ' was ' + verbing + ' through the woods when he encountered a ' + container + '. It was ' + adjective2 + ' and ' + shape + ' and he was surprised to see such a ' + adjective3 + ' object out there in the woods. He peeked inside and saw it contained a ' + smallAnimal +'. <br> "Hello, ' + smallAnimal + ',\" the ' + profession + ' said. <br> To his suprise, the ' + smallAnimal + ' responded. \"Hello, friend! Would you be so kind as to carry me and my ' + container + ' to the next town? I am too ' + adjective4 + ' to carry this ' + container + ' myself.\" <br> The ' + profession + ' obliged, and together he and the ' + smallAnimal + ' journeyed on.'
+}
